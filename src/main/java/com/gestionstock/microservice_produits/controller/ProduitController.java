0756000df38438cc2f8fd70ad4a27b5f698143d5
Serigne Bassirou NDIAYE <dstndiaye@gmail.com>
2025-03-04 18:04:00 +0100

    🧪 Ajout des tests API pour Produit avec Postman et cURL

--- src/main/java/com/gestionstock/microservice_produits/controller/ProduitController.java
+++ src/main/java/com/gestionstock/microservice_produits/controller/ProduitController.java
@@ -1,2 +1,37 @@
 package com.gestionstock.microservice_produits.controller;
 
+
+import com.gestionstock.microservice_produits.model.Produit;
+import com.gestionstock.microservice_produits.service.ProduitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/produits")
+public class ProduitController {
+    @Autowired
+    private ProduitService produitService;
+
+    @GetMapping
+    public List<Produit> getAllProduits() {
+        return produitService.getAllProduits();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produit> getProduitById(@PathVariable Long id) {
+        return produitService.getProduitById(id);
+    }
+
+    @PostMapping
+    public Produit ajouterProduit(@RequestBody Produit produit) {
+        return produitService.ajouterProduit(produit);
+    }
+
+    @DeleteMapping("/{id}")
+    public void supprimerProduit(@PathVariable Long id) {
+        produitService.supprimerProduit(id);
+    }
+}
